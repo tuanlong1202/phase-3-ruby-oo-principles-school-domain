24c31a35f1ceac03fb2be9e95db3d5d635cde376
tuanlong1202 <tuanlong1202@gmail.com>
2021-07-26 21:20:15 -0400

    Phase 3 ruby oo principles school domain

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+    def initialize(school_name)
+        @name = school_name
+        @roster = {}
+    end
+    def add_student(student_name,grade)
+        if @roster.include?(grade)
+            @roster[grade] << student_name
+        else
+            @roster[grade] = []
+            @roster[grade] << student_name
+        end
+    end
+    def grade(grade)
+        @roster[grade]
+    end
+    def sort
+        @roster.sort
+        @roster.each do |grade,students|
+            @roster[grade] = students.sort
+        end
+        @roster
+    end
+end
